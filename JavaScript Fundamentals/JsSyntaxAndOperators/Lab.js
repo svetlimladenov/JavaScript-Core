5fa4c9fb11a5642258608412e77c9c8e50e20894
Svetlin Mladenov <svetlimladenov@gmail.com>
2019-01-15 23:53:30 +0200

    Added lab exercises for js syntax and operator.

---
+++ JavaScript Fundamentals/JsSyntaxAndOperators/Lab.js
@@ -0,0 +1,11 @@
+function solve(){
+        function GetStringLenght(a,b,c){
+            console.log(a.length + b.length + c.length);
+            console.log(Math.floor((a.length + b.length + c.length) / 3));
+
+        }
+
+    
+}
+
+solve();
