eeb25415dd7990c667bd692599f5a875ffc84109
Svetlin Mladenov <svetlimladenov@gmail.com>
2019-01-28 03:27:22 +0200

    Changes dome logic on Cards bug.

--- JavaScript Fundamentals/02.DocumentObjectModel/Exercises/01_Cards/solution.js
+++ JavaScript Fundamentals/02.DocumentObjectModel/Exercises/01_Cards/solution.js
@@ -4,7 +4,7 @@ function solve() {
         card.addEventListener("click", () => {
             card.src = "images/whiteCard.jpg";
             spans[0].textContent = card.getAttribute("name");
-            if (spans[2].textContent)
+            if (spans[2].textContent !== "")
             {
                 Fight(spans[0],spans[2]);
                 card.removeEventListener('click');
@@ -16,7 +16,7 @@ function solve() {
         card.addEventListener("click", () => {
             card.src = "images/whiteCard.jpg";
             spans[2].textContent = card.getAttribute("name");
-            if (spans[0].textContent)
+            if (spans[0].textContent !== "")
             {
                 Fight(spans[0],spans[2]);
                 card.removeEventListener('click');
@@ -28,12 +28,11 @@ function solve() {
         let card1Value = +card1.textContent;
         let card2Value = +card2.textContent;
         let biggestValue = Math.max(card1Value,card2Value);
-        let winnerSelector = 'img[name="'+ biggestValue.toString() +'"]';
-        document.querySelectorAll(winnerSelector)[0].style.border = "2px solid green";
-
+        let winnerSelector = document.getElementsByName(biggestValue.toString());
+        winnerSelector.style.border = "2px solid green";
         let smallestValue = Math.min(card1Value,card2Value);
-        let looserSelector = 'img[name="'+ smallestValue.toString() +'"]';
-        document.querySelectorAll(looserSelector)[0].style.border = "2px solid darkred";
+        let looserSelector = document.getElementsByName(smallestValue.toString());
+        looserSelector.style.border = "2px solid darkred";
 
         setTimeout(() => {
             spans[0].textContent = "";
@@ -41,6 +40,6 @@ function solve() {
         }, 2000);
 
         let history = document.querySelector("#history");
-        history.textContent += "[" + spans[0].textContent + " vs " + spans[2].textContent + "] ";
+        history.textContent += "[" + spans[0].textContent + " vs " + spans[2].text + "] ";
     }
 }
