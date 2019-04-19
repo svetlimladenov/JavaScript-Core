c6edbadf61becdb795d1a50b9d45e777a1525909
Svetlin Mladenov <svetlimladenov@gmail.com>
2019-04-19 16:25:07 +0300

    Fixed requester.js problem with data and content-type and now supports query parameters

--- JavaScript Applications/07.Exam Preparations/Movies Exam 14 April 2019/scripts/models/requester.js
+++ JavaScript Applications/07.Exam Preparations/Movies Exam 14 April 2019/scripts/models/requester.js
@@ -5,48 +5,67 @@ let requester = (() => {
 
     // Creates the authentication header
     function makeAuth(type) {
-        return type === 'basic'
-            ?  'Basic ' + btoa(kinveyAppKey + ':' + kinveyAppSecret)
-            :  'Kinvey ' + sessionStorage.getItem('authtoken');
+        return type === 'basic' ?
+            'Basic ' + btoa(kinveyAppKey + ':' + kinveyAppSecret) :
+            'Kinvey ' + sessionStorage.getItem('authtoken');
     }
 
     // Creates request object to kinvey
-    function makeRequest(method, module, endpoint, auth) {
-        return req = {
-            method,
-            url: kinveyBaseUrl + module + '/' + kinveyAppKey + '/' + endpoint,
-            headers: {
-                'Authorization': makeAuth(auth)
-            }
-        };
+    function makeRequest(method, module, endpoint, auth, query = '') {
+        if (query !== '') {
+            return req = {
+                method,
+                contentType : 'application/json',
+                url: kinveyBaseUrl + module + '/' + kinveyAppKey + '/' + endpoint + '?' + query,
+                headers: {
+                    'Authorization': makeAuth(auth)
+                },
+                
+            };
+        }else{
+            return req = {
+                method,
+                contentType : 'application/json',
+                url: kinveyBaseUrl + module + '/' + kinveyAppKey + '/' + endpoint,
+                headers: {
+                    'Authorization': makeAuth(auth)
+                }
+                
+            };
+        }
     }
 
     // Function to return GET promise
-    function get (module, endpoint, auth) {
+    function get(module, endpoint, auth) {
         return $.ajax(makeRequest('GET', module, endpoint, auth));
     }
 
+    function getWithQuery(module, endpoint, auth, query) {
+        return $.ajax(makeRequest('GET', module, endpoint, auth, query));
+    }
+
     // Function to return POST promise
-    function post (module, endpoint, auth, data) {
+    function post(module, endpoint, auth, data) {
         let req = makeRequest('POST', module, endpoint, auth);
-        req.data = data;
+        req.data = JSON.stringify(data);
         return $.ajax(req);
     }
 
     // Function to return PUT promise
-    function update (module, endpoint, auth, data) {
+    function update(module, endpoint, auth, data) {
         let req = makeRequest('PUT', module, endpoint, auth);
         req.data = data;
         return $.ajax(req);
     }
 
     // Function to return DELETE promise
-    function remove (module, endpoint, auth) {
+    function remove(module, endpoint, auth) {
         return $.ajax(makeRequest('DELETE', module, endpoint, auth));
     }
 
     return {
         get,
+        getWithQuery,
         post,
         update,
         remove
